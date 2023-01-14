ae8a36cd01aa9034ff877db0f1a6dfad19321582
Gabriel Gibran <gagibran@gmail.com>
2023-01-14 20:11:12 -0300

    Added missing migrations

---
+++ src/BookRentalManager.Infrastructure/Data/Migrations/20230114230909_AddedInitialEntities.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookRentalManager.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedInitialEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookAuthor",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FullName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookAuthor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FullName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "character(12)", fixedLength: true, maxLength: 12, nullable: false),
+                    CustomerStatus = table.Column<string>(type: "text", nullable: false),
+                    CustomerPoints = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BookTitle = table.Column<string>(type: "text", nullable: false),
+                    Edition = table.Column<int>(type: "integer", nullable: false),
+                    Isbn = table.Column<string>(type: "character varying(13)", maxLength: 13, nullable: false),
+                    IsAvailable = table.Column<bool>(type: "boolean", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Book_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Book_BookAuthor",
+                columns: table => new
+                {
+                    BookAuthorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    BookId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book_BookAuthor", x => new { x.BookAuthorId, x.BookId });
+                    table.ForeignKey(
+                        name: "FK_Book_BookAuthor_BookAuthor_BookAuthorId",
+                        column: x => x.BookAuthorId,
+                        principalTable: "BookAuthor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Book_BookAuthor_Book_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Book",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_CustomerId",
+                table: "Book",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_BookAuthor_BookId",
+                table: "Book_BookAuthor",
+                column: "BookId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book_BookAuthor");
+
+            migrationBuilder.DropTable(
+                name: "BookAuthor");
+
+            migrationBuilder.DropTable(
+                name: "Book");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
