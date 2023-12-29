4308530d9d2557fa957152caf3e9b7578d0ed6ae
Gabriel Gibran <gabriel.gibran@wexinc.com>
2023-12-29 13:11:19 -0300

    Continued working on integration tests

--- tests/BookRentalManager.IntegrationTests/Api/Controllers/V1/AuthorControllerTests.cs
+++ tests/BookRentalManager.IntegrationTests/Api/Controllers/V1/AuthorControllerTests.cs
@@ -1,14 +1,23 @@
-﻿namespace BookRentalManager.IntegrationTests.Api.Controllers.V1;
+﻿using BookRentalManager.Api.Constants;
+
+namespace BookRentalManager.IntegrationTests.Api.Controllers.V1;
 
 public sealed class AuthorControllerTests(IntegrationTestsWebApplicationFactory integrationTestsWebbApplicationFactory) : IntegrationTest(integrationTestsWebbApplicationFactory)
 {
     [Fact]
     public async Task CreateAuthorAsync_WithMediaTypeVendorSpecific_Returns200WithHateoasLinks()
     {
-        // Act
+        // Arrange:
+        HttpClient.DefaultRequestHeaders.Add("Accept", CustomMediaTypeNames.Application.VendorBookRentalManagerHateoasJson);
+
+        // Act:
         HttpResponseMessage httpResponseMessage = await HttpClient.GetAsync("api/v1/author");
 
-        // Assert
+        // Assert:
         string responseContent = await httpResponseMessage.Content.ReadAsStringAsync();
+        httpResponseMessage.EnsureSuccessStatusCode();
+        Assert.Contains(
+            CustomMediaTypeNames.Application.VendorBookRentalManagerHateoasJson,
+            httpResponseMessage.Content.Headers.ContentType!.ToString());
     }
 }
--- tests/BookRentalManager.IntegrationTests/Common/IntegrationTest.cs
+++ tests/BookRentalManager.IntegrationTests/Common/IntegrationTest.cs
@@ -1,15 +1,7 @@
 ﻿namespace BookRentalManager.IntegrationTests;
 
-public abstract class IntegrationTest : IClassFixture<IntegrationTestsWebApplicationFactory>
+public abstract class IntegrationTest(IntegrationTestsWebApplicationFactory integrationTestsWebbApplicationFactory)
+    : IClassFixture<IntegrationTestsWebApplicationFactory>
 {
-    protected HttpClient HttpClient { get; }
-
-    protected IntegrationTest(IntegrationTestsWebApplicationFactory integrationTestsWebbApplicationFactory)
-    {
-        using IServiceScope serviceScope = integrationTestsWebbApplicationFactory.Services.CreateScope();
-        IServiceProvider serviceProvider = serviceScope.ServiceProvider;
-        BookRentalManagerDbContext bookRentalManagerDbContext = serviceProvider.GetRequiredService<BookRentalManagerDbContext>();
-        bookRentalManagerDbContext.Database.Migrate();
-        HttpClient = integrationTestsWebbApplicationFactory.CreateClient();
-    }
+    protected HttpClient HttpClient { get; } = integrationTestsWebbApplicationFactory.CreateClient();
 }
