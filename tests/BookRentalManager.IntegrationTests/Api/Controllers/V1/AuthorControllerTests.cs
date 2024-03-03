a10fd1e7ca496b48ba2b360201ff30ee0e169a86
Gabriel Alvares Gibran <gagibran@gmail.com>
2024-03-03 14:07:58 -0300

    Improved first integration test

--- tests/BookRentalManager.IntegrationTests/Api/Controllers/V1/AuthorControllerTests.cs
+++ tests/BookRentalManager.IntegrationTests/Api/Controllers/V1/AuthorControllerTests.cs
@@ -5,7 +5,7 @@ namespace BookRentalManager.IntegrationTests.Api.Controllers.V1;
 public sealed class AuthorControllerTests(IntegrationTestsWebApplicationFactory integrationTestsWebbApplicationFactory) : IntegrationTest(integrationTestsWebbApplicationFactory)
 {
     [Fact]
-    public async Task CreateAuthorAsync_WithMediaTypeVendorSpecific_Returns200WithHateoasLinks()
+    public async Task GetAuthorsByQueryParametersAsync_WithMediaTypeVendorSpecific_Returns200WithHateoasLinks()
     {
         // Arrange:
         HttpClient.DefaultRequestHeaders.Add("Accept", CustomMediaTypeNames.Application.VendorBookRentalManagerHateoasJson);
@@ -15,9 +15,15 @@ public sealed class AuthorControllerTests(IntegrationTestsWebApplicationFactory 
 
         // Assert:
         string responseContent = await httpResponseMessage.Content.ReadAsStringAsync();
+        (List<object> values, List<object> links) = GetValuesAndLinksFromHateoasResponse(responseContent);
         httpResponseMessage.EnsureSuccessStatusCode();
         Assert.Contains(
             CustomMediaTypeNames.Application.VendorBookRentalManagerHateoasJson,
             httpResponseMessage.Content.Headers.ContentType!.ToString());
+        Assert.NotEqual(string.Empty, ((dynamic)values.ElementAt(0)).id);
+        Assert.Equal("Erich Gamma", ((dynamic)values.ElementAt(0)).fullName);
+        Assert.NotEmpty(((dynamic)values.ElementAt(0)).books);
+        Assert.NotEmpty(((dynamic)values.ElementAt(0)).links);
+        Assert.Empty(links);
     }
 }
--- tests/BookRentalManager.IntegrationTests/Common/IntegrationTest.cs
+++ tests/BookRentalManager.IntegrationTests/Common/IntegrationTest.cs
@@ -1,7 +1,18 @@
-﻿namespace BookRentalManager.IntegrationTests;
+﻿using System.Dynamic;
+using Newtonsoft.Json;
+
+namespace BookRentalManager.IntegrationTests;
 
 public abstract class IntegrationTest(IntegrationTestsWebApplicationFactory integrationTestsWebbApplicationFactory)
     : IClassFixture<IntegrationTestsWebApplicationFactory>
 {
     protected HttpClient HttpClient { get; } = integrationTestsWebbApplicationFactory.CreateClient();
+
+    protected static (List<object> values, List<object> links) GetValuesAndLinksFromHateoasResponse(string responseContent)
+    {
+        ExpandoObject? responseBody = JsonConvert.DeserializeObject<ExpandoObject>(responseContent);
+        var values = (List<object>?)responseBody!.ElementAt(0).Value;
+        var links = (List<object>?)responseBody!.ElementAt(1).Value;
+        return (values!, links!);
+    }
 }
